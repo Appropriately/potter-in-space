a55ae743cefa75125297bb8fcf4f521ff971f657
Sean Lewis <sean.alan.lewis@gmail.com>
2017-05-15 13:37:09 +0000

    Fixed the code, made the movement smoother and added random images

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 var iterations = 0;
+var elise = document.getElementById("elise");
+
+var width;
+var height;
+
+var backgrounds = [
+    "url(backgrounds/wallpaper.jpg)", "url(backgrounds/wallpaper1.jpg)",
+    "url(backgrounds/image.png)", "url(backgrounds/test.jpg)"
+];
+
+
 
 function magic() {
-  var elise = document.getElementById("elise");
-
-  var width = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
-
-  var height = window.innerHeight
-  || document.documentElement.clientHeight
-  || document.body.clientHeight;
-
   setup(elise, width, height);
-  setInterval(function(){setup(elise, width, height)},10000);
 } // magic
 
 function setup(elise, width, height) {
+  var randomValue = Math.floor(Math.random()*backgrounds.length);
+  document.body.style.backgroundImage = backgrounds[randomValue];
+
   var randomX, randomY, posX, posY;
   iterations = 0;
+  height = window.innerHeight
+  || document.documentElement.clientHeight
+  || document.body.clientHeight;
+
+  width = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
 
   // Generate random position
   posY = elise.style.top = Math.random() * height;
   posX = elise.style.left = Math.random() * width;
 
   // Generate some values
-  randomX = Math.random() * 2;
-  randomY = Math.random() * 2;
+  randomX = generateNumber();
+  randomY = generateNumber();
 
   // Do the movement
-  setInterval(function(){movement(elise, posX, posY,randomX,randomY)}, 200);
+  setTimeout(function(){movement(elise, posX, posY,randomX,randomY)}, 50);
 } // setup
 
 // The function is passed values and then moves by those appropriate values
 function movement(elise,x,y,ranX,ranY) {
-  elise.style.top  = (y+(iterations*ranY)) + 'px';
-  elise.style.left = (x+(iterations*ranX)) + 'px';
-  console.log(iterations);
-  iterations++;
+  if (iterations < 200) {
+    elise.style.top  = (y+(iterations*ranY)) + 'px';
+    elise.style.left = (x+(iterations*ranX)) + 'px';
+    console.log(iterations);
+    iterations++;
+    setTimeout(function(){movement(elise, x, y,ranX,ranY)}, 50);
+  } else {
+    setTimeout(function(){setup(elise, width, height)},10);
+  } // if/else
 } // movement
+
+function generateNumber() {
+  return -2 + (Math.random() * 4);
+} // generateNumber
